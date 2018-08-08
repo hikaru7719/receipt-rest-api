b776285bd92c7c4d020ae32cab583645b70034b3
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2018-08-09 00:00:32 +0900

    go fmt

--- infrastructure/datastore/connection.go
+++ infrastructure/datastore/connection.go
@@ -1,18 +1,18 @@
 package datastore
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"os"
-	"fmt"
 )
 
 func GetConnection() *gorm.DB {
 	user := os.Getenv("MYSQL_USER")
 	pass := os.Getenv("MYSQL_PASS")
 	dbname := os.Getenv("MYSQL_DBNAME")
-	fmt.Println(user,pass,dbname)
-	connect := user+ ":" + pass + "@" + "tcp(127.0.0.1:3306)" + "/" + dbname
+	fmt.Println(user, pass, dbname)
+	connect := user + ":" + pass + "@" + "tcp(127.0.0.1:3306)" + "/" + dbname
 	db, err := gorm.Open("mysql", connect)
 
 	if err != nil {
--- infrastructure/datastore/receipt_repository_test.go
+++ infrastructure/datastore/receipt_repository_test.go
@@ -1,19 +1,19 @@
 package datastore
 
 import (
-	"testing"
 	"github.com/hikaru7719/receipt-rest-api/domain/model"
 	"reflect"
+	"testing"
 )
 
-func TestReceiptRepository_FindOne(t *testing.T){
+func TestReceiptRepository_FindOne(t *testing.T) {
 	db := GetConnection()
-	expected := &model.Receipt{ID:1, Name:"test",Kind:"日用品",Date:"2018-08-08",Memo:"test"}
+	expected := &model.Receipt{ID: 1, Name: "test", Kind: "日用品", Date: "2018-08-08", Memo: "test"}
 	tx := db.Begin()
 	repositpory := NewReceiptRepository(tx)
 	tx.Create(&expected)
-	actual,_ := repositpory.FindOne(1)
-	if !reflect.DeepEqual(actual,expected){
+	actual, _ := repositpory.FindOne(1)
+	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("actual %v\nwant %v", actual, expected)
 	}
 
