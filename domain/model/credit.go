a5abeaece02c0def65964ece279e4b6203a22727
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2018-08-15 23:04:47 +0900

    create credit card

---
+++ domain/model/credit.go
@@ -0,0 +1,14 @@
+package model
+
+// Credit -　クレジットカードを表す構造体
+type Credit struct {
+	ID             int
+	CardName       string
+	OpenigDate     string
+	ClosingDate    string
+	WithdrawalDate string
+}
+
+func NewCredit(cardName, openingDate, closingDate, withdrawalDate string) (*Credit, error) {
+	return &Credit{CardName: cardName, OpenigDate: openingDate, ClosingDate: closingDate, WithdrawalDate: withdrawalDate},nil
+}
