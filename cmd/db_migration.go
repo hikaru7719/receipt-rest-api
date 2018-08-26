8a236f9affeed14ff7c24bd81509ad181c14a730
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2018-08-26 23:36:59 +0900

    構造体の変更

---
+++ cmd/db_migration.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/hikaru7719/receipt-rest-api/infrastructure/datastore"
+	"github.com/hikaru7719/receipt-rest-api/domain/model"
+)
+
+func main(){
+	datastore.CreateConnection(datastore.GetDBEnv())
+	datastore.DB.AutoMigrate(&model.Credit{})
+	datastore.DB.AutoMigrate(&model.Receipt{})
+}
