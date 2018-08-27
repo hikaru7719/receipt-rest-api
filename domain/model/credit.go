2d708b27ed539d465645c08bce90a78d877354c3
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2018-08-27 22:38:20 +0900

    credit repositoryの作成

--- domain/model/credit.go
+++ domain/model/credit.go
@@ -2,15 +2,15 @@ package model
 
 // Credit -　クレジットカードを表す構造体
 type Credit struct {
-	ID             int `gorm:"primary_key"`
-	CardName       string
-	OpeningDate    string
-	ClosingDate    string
-	WithdrawalDate string
-	PaymentMonth   string
+	ID                int    `json:"id" gorm:"primary_key"`
+	UserID            int    `json:"user_id"`
+	CardName          string `json:"card_name"`
+	FinishDate        int    `json:"finish_date"`
+	WithdrawalDate    int `json:"withdrawal_date"`
+	LaterPaymentMonth int `json:"later_payment_month"`
 }
 
 // NewCredit - クレジットカードの生成
-func NewCredit(cardName, openingDate, closingDate, withdrawalDate string) (*Credit, error) {
-	return &Credit{CardName: cardName, OpeningDate: openingDate, ClosingDate: closingDate, WithdrawalDate: withdrawalDate}, nil
+func NewCredit(userID int, cardName string, finishDate, withdrawalDate, laterPaymentMonth int) (*Credit, error) {
+	return &Credit{UserID: userID, CardName: cardName,FinishDate:finishDate, WithdrawalDate: withdrawalDate, LaterPaymentMonth:laterPaymentMonth}, nil
 }
--- domain/repository/credit_repository.go
+++ domain/repository/credit_repository.go
@@ -1,1 +1,8 @@
 package repository
+
+import "github.com/hikaru7719/receipt-rest-api/domain/model"
+
+// CreditRepository - クレジットカードに対応するリポジトリのインターフェース
+type CreditRepository interface {
+	Create(credit *model.Credit) (*model.Credit, error)
+}
