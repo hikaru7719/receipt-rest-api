b9537cab9cab82201aa2676e4ef8c80e617801b8
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2018-08-31 08:48:35 +0900

    API testの修正

--- interface/server/router/main_test.go
+++ interface/server/router/main_test.go
@@ -12,30 +12,64 @@ import (
 	"testing"
 )
 
-func TestRouter(t *testing.T) {
+func TestAPI(t *testing.T) {
 	datastore.CreateConnection(datastore.GetDBEnv())
-	testData, _ := model.NewReceipt(1, "test", 1000, "日用品", "2018-08-08", "memo", 1)
-	datastore.DB.Create(testData)
 	r := router()
 	testServer := httptest.NewServer(r)
 	client := new(http.Client)
+	testGetReceipt(t, client, testServer)
+	testPostReceipt(t, client, testServer)
+	testDeleteReceipt(t, client, testServer)
+	testPostCredit(t, client, testServer)
+}
+
+func testGetReceipt(t *testing.T, client *http.Client, testServer *httptest.Server) {
+	testData, _ := model.NewReceipt(1, "test", 1000, "日用品", "2018-08-08", "memo", 1)
+	datastore.DB.Create(testData)
 	id := strconv.Itoa(testData.ID)
 	req, _ := http.NewRequest("GET", testServer.URL+"/v1/receipt/"+id, nil)
 	res, _ := client.Do(req)
 
 	if res.StatusCode != 200 {
-		t.Error("Get Request Not Working")
+		t.Error("Get Request /v1/receipt/:id Not Working")
 	}
+}
 
-	test := &form.ReceiptForm{UserID: 2, Name: "test", Price: 1000, Kind: "日用品", Date: "2018-08-08", Memo: "test", CreditID: 2}
+func testPostReceipt(t *testing.T, client *http.Client, testServer *httptest.Server) {
+	testData := &form.ReceiptForm{UserID: 2, Name: "test", Price: 1000, Kind: "日用品", Date: "2018-08-08", Memo: "test", CreditID: 2}
 	buf := new(bytes.Buffer)
-	json.NewEncoder(buf).Encode(test)
-	postReq, _ := http.NewRequest("POST", testServer.URL+"/v1/receipt", buf)
-	postReq.Header.Set("Content-Type", "application/json")
-	postRes, _ := client.Do(postReq)
+	json.NewEncoder(buf).Encode(testData)
+	req, _ := http.NewRequest("POST", testServer.URL+"/v1/receipt", buf)
+	req.Header.Set("Content-Type", "application/json")
+	res, _ := client.Do(req)
 
-	if postRes.StatusCode != 201 {
-		t.Error("Post Request Not Working")
+	if res.StatusCode != 201 {
+		t.Error("Post Request /v1/receipt Not Working")
+	}
+}
+
+func testDeleteReceipt(t *testing.T, client *http.Client, testServer *httptest.Server) {
+	testData, _ := model.NewReceipt(3, "test", 1000, "日用品", "2018-08-08", "memo", 1)
+	datastore.DB.Create(testData)
+	id := strconv.Itoa(testData.ID)
+	req, _ := http.NewRequest("DELETE", testServer.URL+"/v1/receipt/"+id, nil)
+	res, _ := client.Do(req)
+
+	if res.StatusCode != 202 {
+		t.Error("Delete Request /v1/receipt/:id Not Working")
 	}
 
+}
+
+func testPostCredit(t *testing.T, client *http.Client, testServer *httptest.Server) {
+	testData := &form.CreditForm{UserID: 1, CardName: "アメリカンエクスプレス", FinishDate: 10, WithdrawalDate: 4, LaterPaymentMonth: 1}
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(testData)
+	req, _ := http.NewRequest("POST", testServer.URL+"/v1/credit", buf)
+	req.Header.Set("Content-Type", "application/json")
+	res, _ := client.Do(req)
+
+	if res.StatusCode != 201 {
+		t.Error("Post Request /v1/credit Not Working")
+	}
 }
