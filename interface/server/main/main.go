b602fd9e4895faf8a3f1369cbd3bb4a6c3d3bc8d
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2018-09-09 01:08:38 +0900

    main関数の簡略化

--- interface/server/main/main.go
+++ interface/server/main/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"github.com/hikaru7719/receipt-rest-api/infrastructure/datastore"
 	"github.com/hikaru7719/receipt-rest-api/interface/server/router"
 )
 
 func main() {
 
 	r := router.Router()
-	datastore.CreateConnection(datastore.GetDBEnv())
 	r.Run(":8080")
 
 }
--- interface/slack/main/main_slack.go
+++ interface/slack/main/main_slack.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"github.com/hikaru7719/receipt-rest-api/infrastructure/datastore"
 	"github.com/hikaru7719/receipt-rest-api/interface/slack/router"
 )
 
 func main() {
-	connect := datastore.GetDBEnv()
 	r := router.SlackRouter()
-	datastore.CreateConnection(connect)
 	r.Run(":8080")
 }
